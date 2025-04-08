09472ee7f9c0c11940b76db23f99601aa43ebe4c
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-04-08 11:18:50 -0400

    Refactor useToggle hook to implement getTogglerProps function, enhancing toggle functionality by allowing custom onClick handling and improving accessibility with aria-checked property.

--- exercises/05.prop-getters/02.problem.getters/toggle.tsx
+++ exercises/05.prop-getters/02.problem.getters/toggle.tsx
@@ -1,23 +1,27 @@
 import { useState } from 'react'
 
+function callAll<Args extends Array<unknown>>(
+	...fns: Array<((...args: Args) => unknown) | undefined>
+) {
+	return (...args: Args) => fns.forEach((fn) => fn?.(...args))
+}
+
 export function useToggle() {
 	const [on, setOn] = useState(false)
 	const toggle = () => setOn(!on)
 
-	// 🐨 create a function called getTogglerProps that accepts an object
-	// of props and returns an object of props that includes 'aria-checked' and onClick.
-
-	// 💰 Make sure to handle the case where the user provides their own
-	// 'aria-checked' and 'onClick' props (as well as if they don't or if they
-	// provide more props).
+	function getTogglerProps<Props>({
+		onClick,
+		...props
+	}: {
+		onClick?: React.ComponentProps<'button'>['onClick']
+	} & Props) {
+		return { 'aria-checked': on, onClick: callAll(onClick, toggle), ...props }
+	}
 
 	return {
 		on,
 		toggle,
-		// 🐨 swap togglerProps with getTogglerProps
-		togglerProps: {
-			'aria-checked': on,
-			onClick: toggle,
-		},
+		getTogglerProps,
 	}
 }
