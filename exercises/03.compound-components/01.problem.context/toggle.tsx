fb49da3e387870dbb8cdbd415edd71715f0412ac
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-04-07 20:01:43 -0400

    Implement ToggleContext for managing toggle state across components, enhancing the Toggle, ToggleOn, ToggleOff, and ToggleButton components to utilize context for state management.

--- exercises/03.compound-components/01.problem.context/toggle.tsx
+++ exercises/03.compound-components/01.problem.context/toggle.tsx
@@ -1,44 +1,31 @@
-import { useState } from 'react'
+import { createContext, use, useState } from 'react'
 import { Switch } from '#shared/switch.tsx'
 
-// 🐨 create your ToggleContext context here
-// 📜 https://react.dev/reference/react/createContext
-// 💰 the default value should be `null`
-// 🦺 the typing for the context value should be `{on: boolean; toggle: () => void}`
-// but because we must initialize it to `null`, you need to union that with `null`
+type ToggleValue = { on: boolean; toggle: () => void }
+const ToggleContext = createContext<ToggleValue | null>(null)
 
 export function Toggle({ children }: { children: React.ReactNode }) {
 	const [on, setOn] = useState(false)
 	const toggle = () => setOn(!on)
-
-	// 💣 remove this and instead return <ToggleContext> where
-	// the value is an object that has `on` and `toggle` on it. Render children
-	// within the provider.
-	return <>TODO...</>
+	return <ToggleContext value={{ on, toggle }}>{children}</ToggleContext>
 }
 
 export function ToggleOn({ children }: { children: React.ReactNode }) {
-	// 🐨 instead of this constant value, we'll need to get that from
-	// use(ToggleContext)
-	// 📜 https://react.dev/reference/react/use#reading-context-with-use
-	const on = false
+	const { on } = use(ToggleContext)!
 	return <>{on ? children : null}</>
 }
 
 export function ToggleOff({ children }: { children: React.ReactNode }) {
-	// 🐨 do the same thing to this that you did to the ToggleOn component
-	const on = false
+	const { on } = use(ToggleContext)!
 	return <>{on ? null : children}</>
 }
 
 type ToggleButtonProps = Omit<React.ComponentProps<typeof Switch>, 'on'> & {
 	on?: boolean
 }
-export function ToggleButton(props: ToggleButtonProps) {
-	// 🐨 get `on` and `toggle` from the ToggleContext with `use`
-	const on = false
-	const toggle = () => {}
-	return <Switch on={on} onClick={toggle} {...props} />
+export function ToggleButton({ ...props }: ToggleButtonProps) {
+	const { on, toggle } = use(ToggleContext)!
+	return <Switch {...props} on={on} onClick={toggle} />
 }
 
 /*
