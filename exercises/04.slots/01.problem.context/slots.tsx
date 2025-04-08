a7fffb74f64f3464df99c50b7daa98ce60c1176d
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-04-07 21:24:11 -0400

    Refactor Slots component to implement SlotContext and useSlotProps hook for improved prop management in Label and Input components.

--- exercises/04.slots/01.problem.context/slots.tsx
+++ exercises/04.slots/01.problem.context/slots.tsx
@@ -1,19 +1,19 @@
-// 🦺 create a Slots type that's just an object of objects
-// 💰 type Slots = Record<string, Record<string, unknown>>
-// 🐨 create and export a SlotContext with that type and default it to an empty object
+import { createContext, use } from 'react'
 
-// 🐨 create a useSlotProps hook which:
-// 1. accepts props (any type) and slot (string)
-// 2. gets the slots from the SlotContext
-// 3. gets the props from the slot by its name
-// 4. returns the merged props with the slot and given props
+type Slots = Record<string, Record<string, unknown>>
+export const SlotContext = createContext<Slots>({})
+
+function useSlotProps<Props>(props: Props, slot: string): Props {
+	const slots = use(SlotContext)
+	return { ...slots[slot], slot, ...props } as Props
+}
 
 export function Label(props: React.ComponentProps<'label'>) {
-	// 🐨 get the props from useSlotProps for a slot called "label" and apply those to the label
+	props = useSlotProps(props, 'label')
 	return <label {...props} />
 }
 
 export function Input(props: React.ComponentProps<'input'>) {
-	// 🐨 get the props from useSlotProps for a slot called "input" and apply those to the input
+	props = useSlotProps(props, 'input')
 	return <input {...props} />
 }
