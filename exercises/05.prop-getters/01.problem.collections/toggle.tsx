f1a61da1e0ce20128b7c62fbebc9a8379855f3a8
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-04-08 10:08:16 -0400

    Enhance useToggle hook to include togglerProps with aria-checked and onClick properties, improving accessibility and functionality of the toggle component.

--- exercises/05.prop-getters/01.problem.collections/toggle.tsx
+++ exercises/05.prop-getters/01.problem.collections/toggle.tsx
@@ -4,7 +4,5 @@ export function useToggle() {
 	const [on, setOn] = useState(false)
 	const toggle = () => setOn(!on)
 
-	// 🐨 Add a property called `togglerProps`. It should be an object that has
-	// `aria-checked` and `onClick` properties.
-	return { on, toggle, togglerProps: {} }
+	return { on, toggle, togglerProps: { 'aria-checked': on, onClick: toggle } }
 }
