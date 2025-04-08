c2949eddcaae7b236445492c5d7506aaff3ffca0
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-04-07 21:35:33 -0400

    Refactor App component to replace label with Label component from slots.tsx and remove unused partyModeId variable, streamlining the toggle functionality.

--- exercises/04.slots/02.problem.generic/app.tsx
+++ exercises/04.slots/02.problem.generic/app.tsx
@@ -1,19 +1,14 @@
-import { useId } from 'react'
 import { Input, Label } from './slots.tsx'
 import { TextField } from './text-field.tsx'
 import { Toggle, ToggleButton, ToggleOff, ToggleOn } from './toggle.tsx'
 
 export function App() {
-	// 💣 delete this variable
-	const partyModeId = useId()
 	return (
 		<div>
 			<div>
 				<Toggle>
-					{/* 🐨 switch this label for the Label component from ./slots.tsx */}
-					<label htmlFor={partyModeId}>Party mode</label>
-					{/* 🐨 remove this id prop */}
-					<ToggleButton id={partyModeId} />
+					<Label>Party mode</Label>
+					<ToggleButton />
 					<ToggleOn>Let's party 🥳</ToggleOn>
 					<ToggleOff>Sad town 😭</ToggleOff>
 				</Toggle>
